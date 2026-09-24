98a2839ae823618e358b1d7628fd7a28186077a7
rejoreji <rejoreji03@gmail.com>
2026-09-24 11:11:15 +0530

    First commit

---
+++ test_app.py
@@ -0,0 +1,17 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert b"Hello, GitHub Actions!" in response.data
+
+
+def test_about():
+    client = app.test_client()
+    response = client.get("/about")
+
+    assert response.status_code == 200
+    assert b"This is my Flask application." in response.data
